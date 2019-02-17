f679eb1ebd6336f4b6a1311172f57fccbe0504d2
Pratik Prabhu <prabhupratik27@gmail.com>
2019-02-17 16:35:30 +0530

    Completed lesson 10

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
 new Vue({
     el: "#vue-app",
     data: {
-        a: 0,
-        b: 0,
-        age: 20
+        available: false,
+        nearby: false
     },
     methods: {
-        
+
     },
-    computed:{
-        addA: function () {
-            console.log("Add to A");
-            return this.a + this.age;
-        },
-        addB: function () {
-            console.log("Add to B");
-            return this.b + this.age;
+    computed: {
+        compClasses: function(){
+            return {
+                available: this.available,
+                nearby: this.nearby
+            }
         }
     }
 });
