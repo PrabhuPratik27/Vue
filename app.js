c3bf97abc3da4058df6de3039d3149d7c16d72f0
Pratik Prabhu <prabhupratik27@gmail.com>
2019-02-17 12:32:21 +0530

    Completed lesson 8

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
 new Vue({
     el: "#vue-app",
     data: {
-        name : "",
-        age: 0
+        name: "",
+        age: ""
     },
     methods: {
-        logName : function(){
-            console.log("You entered your name");
-        },
-        logAge : function(){
-            console.log("You entered your age");
-        }
+
     }
 });
